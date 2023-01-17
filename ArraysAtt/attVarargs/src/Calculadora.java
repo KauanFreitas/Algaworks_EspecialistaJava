d2d40b565ea0549c9a83afb7b66220252786b6ac
Kauan Freitas <kauan.allexandre@gmail.com>
2023-01-17 12:14:05 -0300

    Desafil Varargs

---
+++ ArraysAtt/attVarargs/src/Calculadora.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Calculadora {
+
+    static double cacularMedia(double numeroA, double numeroB, double... demaisNumeros){
+        double[] todosNumeros = Arrays.copyOf(demaisNumeros, demaisNumeros.length+2);
+        todosNumeros[todosNumeros.length -2] = numeroA;
+        todosNumeros[todosNumeros.length -1]= numeroB;
+
+        double soma =0;
+        for (double numero : todosNumeros) {
+            soma += numero;
+        }
+        return  soma/ todosNumeros.length;
+    }
+
+}
+
---
+++ ArraysAtt/attVarargs/src/PrincipalAtt.java
@@ -0,0 +1,9 @@
+public class PrincipalAtt {
+
+    public static void main(String[] args) {
+        double media = Calculadora.cacularMedia(59.2, 89.2,
+                10.2);
+
+        System.out.printf("Média: %.2f%n", media);
+    }
+}
