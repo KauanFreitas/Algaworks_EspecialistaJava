7a138fff648b50bda6399a4bacee085594112977
Kauan Freitas <kauan.allexandre@gmail.com>
2023-01-17 10:14:22 -0300

    Iterando em arrayas multidimensionais

--- ArraysAtt/arrayDimencoes/src/MultiDimencao.java
+++ ArraysAtt/arrayDimencoes/src/MultiDimencao.java
@@ -1,16 +1,31 @@
+import java.util.Arrays;
+
 public class MultiDimencao {
     public static void main(String[] args) {
-        String[][] cidades = new String[3][];
-        cidades [0] = new String[3];
-        cidades[0][0] = "Uberlandia";
-        cidades[0][1] = "Uberaba";
-        cidades[0][2] = "Belo Horizonte";
+        String[][] todasCidades = new String[3][];
+        todasCidades [0] = new String[3];
+        todasCidades[0][0] = "Uberlandia";
+        todasCidades[0][1] = "Uberaba";
+        todasCidades[0][2] = "Belo Horizonte";
 
-        cidades [1] = new String[2];
-        cidades[1][0] = "São Paulo";
-        cidades[1][1] = "Ribeirao Preto";
+        todasCidades [1] = new String[2];
+        todasCidades[1][0] = "São Paulo";
+        todasCidades[1][1] = "Ribeirao Preto";
 
-        cidades [2] = new String[1];
-        cidades[2][0] = "Fortaleza";
+        todasCidades [2] = new String[1];
+        todasCidades[2][0] = "Fortaleza";
+
+//        for (int i = 0; i < todasCidades.length; i++) {
+//            for (int j = 0; j < todasCidades[i].length; j++) {
+//                    System.out.printf("[%d][%d] = %s%n", i, j, todasCidades[i][j]);
+//                System.out.println(todasCidades[i][j]);
+//
+//            }
+//        }
+        for (String[] cidadesPorEstados : todasCidades) {
+            for (String cidade : cidadesPorEstados) {
+                System.out.println(cidade);
+            }
+        }
     }
 }
