41e6166175d51f48785e32f89f8e93c09b9b8216
Kauan Freitas <kauan.allexandre@gmail.com>
2023-01-23 10:13:19 -0300

    Construtores com Parâmetros

---
+++ construtores/construtor_parametros/src/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto("Picanha 1kg (paça)");
+
+
+
+        System.out.println(produto1.nome);
+        System.out.println(produto1.quantidadeEstoque);
+    }
+
+}
