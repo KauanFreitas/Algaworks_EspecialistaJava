7fce89f67388630036392994fdba9cf9ef5d374e
Kauan Freitas <kauan.allexandre@gmail.com>
2023-01-23 10:48:50 -0300

    Encadeamento de chamadas de construtores

---
+++ construtores/encadeamento_chamadas_construtores/src/Produto.java
@@ -0,0 +1,28 @@
+import java.util.Objects;
+
+public class Produto {
+
+    static final  int QUANTIDADE_ESTOQUE_INICIAL = 100;
+
+    String nome;
+    int quantidadeEstoque;
+
+    Produto(){
+        this("Sem nome");
+    }
+
+    Produto(String nome){
+        this(nome,QUANTIDADE_ESTOQUE_INICIAL);
+    }
+    Produto(String nome, int estoqueInicial){
+        Objects.requireNonNull(nome, "nome é obrigatorio");
+
+        if(estoqueInicial<0){
+            throw new IllegalArgumentException("Estoque inicial não pode ser negativo");
+        }
+
+        this.nome = nome;
+        this.quantidadeEstoque = estoqueInicial;
+    }
+
+}
