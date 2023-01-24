3863570026e98034c074b62e5b742506bc4109d1
Kauan Freitas <kauan.allexandre@gmail.com>
2023-01-23 22:12:27 -0300

    importando membros estáticos

---
+++ matematica-util/src/com/github/KauanFreitas/Principal.java
@@ -0,0 +1,14 @@
+package com.github.KauanFreitas;
+
+import static com.github.KauanFreitas.matematica.CalculadoraArea.*;
+
+public class Principal {
+    public static void main(String[] args) {
+        double areaQuadrado = calcularAreaQuadrado(5.2);
+        double areaCirculo = calcularAreaCirculo(10.5);
+
+        System.out.printf("PI: %.2f%n", PI);
+        System.out.printf("Área do quadrado: %.2f%n", areaQuadrado);
+        System.out.printf("Área do círculo: %.2f%n", areaCirculo);
+    }
+}
