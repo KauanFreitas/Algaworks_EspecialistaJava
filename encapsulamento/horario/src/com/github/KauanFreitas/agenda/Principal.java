aaa7d13a8fcbb3cb9690d372e7e9c3bad0a6bbd6
Kauan Freitas <kauan.allexandre@gmail.com>
2023-01-27 13:35:03 -0300

    Boas práticas: use métodos de acesso em classes públicas (incluindo Tell Don't Ask)

---
+++ encapsulamento/horario/src/com/github/KauanFreitas/agenda/Principal.java
@@ -0,0 +1,10 @@
+package com.github.KauanFreitas.agenda;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Horario horario = new Horario(10, 55);
+//        System.out.printf("%dh%dm", horario.getHora(), horario.getMinuto());
+        System.out.println(horario.formatar());
+    }
+}
