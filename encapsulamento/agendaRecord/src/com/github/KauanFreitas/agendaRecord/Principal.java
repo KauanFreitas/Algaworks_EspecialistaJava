a37136568b963baaa5e76ef109524c0d94c2d6a7
Kauan Freitas <kauan.allexandre@gmail.com>
2023-02-01 14:10:54 -0300

    Records

---
+++ encapsulamento/agendaRecord/src/com/github/KauanFreitas/agendaRecord/Principal.java
@@ -0,0 +1,13 @@
+package com.github.KauanFreitas.agendaRecord;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Horario horario = new Horario(10, 30);
+
+        Agendamento agendamentoCabelo = new Agendamento(horario, "Corte de cabelo");
+        agendamentoCabelo.setHorario(new Horario(16, 20));
+
+        System.out.println(agendamentoCabelo.getHorarioFormatado());
+    }
+}
