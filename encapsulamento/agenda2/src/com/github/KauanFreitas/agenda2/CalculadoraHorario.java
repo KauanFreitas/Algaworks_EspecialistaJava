a4aec501eb136ed219f543c8cf6467f699341f39
Kauan Freitas <kauan.allexandre@gmail.com>
2023-02-01 13:59:49 -0300

    Boas práticas: minimize a mutabilidade (incluindo Value Object)

---
+++ encapsulamento/agenda2/src/com/github/KauanFreitas/agenda2/CalculadoraHorario.java
@@ -0,0 +1,19 @@
+package com.github.KauanFreitas.agenda2;
+
+public class CalculadoraHorario {
+    private CalculadoraHorario() {
+    }
+
+    public static Horario somarDuasHoras(Horario horario) {
+        int hora = horario.getHora() + 2;
+
+        if (hora > 24) {
+            hora = hora - 24;
+        }
+
+//        horario.setHora(hora);
+//        return horario;
+
+        return new Horario(hora, horario.getMinuto());
+    }
+}
---
+++ encapsulamento/agenda2/src/com/github/KauanFreitas/agenda2/Principal1.java
@@ -0,0 +1,12 @@
+package com.github.KauanFreitas.agenda2;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        Integer idade = 30;
+        Integer novaIdade = idade + 1;
+
+        Horario horario = new Horario(10, 30);
+//        horario.setHora(15);
+    }
+}
