b1171d088bc8970f490beba4602d90323685b8a2
Kauan Freitas <kauan.allexandre@gmail.com>
2023-02-02 14:20:52 -0300

    Chamando método da superclasse com super

--- banco/src/com/github/KauanFreitas/banco/ContaEspecial.java
+++ banco/src/com/github/KauanFreitas/banco/ContaEspecial.java
@@ -25,13 +25,10 @@ public class ContaEspecial extends ContaInvestimento {
     }
     @Override
     public void imprimirDemonstrativo(){
-        System.out.println();
-        System.out.printf("Agência: %d%n", getAgencia());
-        System.out.printf("Conta: %d%n", getNumero());
-        System.out.printf("Titular: %s%n", getTitular().getNome());
-        System.out.printf("Saldo: %.2f%n", getSaldo());
+        super.imprimirDemonstrativo();
         System.out.printf("Saldo Disponível: %.2f%n", getSaldoDisponivel());
     }
+
     @Override
     protected void validarSaldoParaSaque(double valorSaque) {
         if (getSaldoDisponivel() < valorSaque) {
