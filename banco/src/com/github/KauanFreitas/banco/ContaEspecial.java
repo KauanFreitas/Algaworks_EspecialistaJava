d0d17ebe0dffe7a68ee22fe5beccd45ca6f385fc
Kauan Freitas <kauan.allexandre@gmail.com>
2023-02-02 13:39:07 -0300

    Sobrescrita de métodos

--- banco/src/com/github/KauanFreitas/banco/ContaEspecial.java
+++ banco/src/com/github/KauanFreitas/banco/ContaEspecial.java
@@ -20,6 +20,30 @@ public class ContaEspecial extends ContaInvestimento {
     public void setLimiteChequeEspecial(double limiteChequeEspecial) {
         this.limiteChequeEspecial = limiteChequeEspecial;
     }
+    public double getSaldoDisponivel(){
+        return getSaldo() + getSaldoDisponivel();
+    }
+    public void imprimirDemostrativo(){
+        System.out.println();
+        System.out.printf("Agência: %d%n", getAgencia());
+        System.out.printf("Conta: %d%n", getNumero());
+        System.out.printf("Titular: %s%n", getTitular().getNome());
+        System.out.printf("Saldo: %.2f%n", getSaldo());
+        System.out.printf("Saldo Disponível: %.2f%n", getSaldoDisponivel());
+    }
+
+    public void sacar(double valorSaque) {
+        if (valorSaque <= 0) {
+            throw new IllegalArgumentException("Valor do saque deve ser maior que 0");
+        }
+
+        if (getSaldoDisponivel() < valorSaque) {
+            throw new RuntimeException("Saldo insuficiente para saque");
+        }
+
+        Saldo -= valorSaque;
+    }
+
 
     public void debitarTarifaMensal(){
             sacar(getTarifaMensal());
